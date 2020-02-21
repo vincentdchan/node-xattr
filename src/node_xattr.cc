8da47e314dabd210d6bc652edd7e660fbd62e78c
Vincent Chan <okcdz@diverse.space>
2020-02-21 19:03:29 +0800

    feat: implement listxattr

--- src/node_xattr.cc
+++ src/node_xattr.cc
@@ -1,6 +1,48 @@
 #include <napi.h>
 #include <vector>
+#include <cstdlib>
+#include <cstring>
 #include <sys/xattr.h>
+
+static Napi::Array ListXattrSync(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+
+  if (!info[0].IsString()) {
+    throw Napi::Error::New(env, "The first argument must be a string");
+  }
+
+  std::string path = info[0].As<Napi::String>().Utf8Value();
+
+  ssize_t buffer_size = listxattr(path.c_str(), nullptr, 0, 0);
+  if (buffer_size < 0) {
+    std::string message = strerror(errno);
+    throw Napi::Error::New(env, message);
+  }
+
+  Napi::Array result = Napi::Array::New(env);
+
+  std::vector<char> buffer;
+  buffer.resize(buffer_size);
+  char* buffer_ptr = buffer.data();
+
+  ssize_t ret = listxattr(path.c_str(), buffer_ptr, buffer_size, 0);
+  if (ret < 0) {
+    std::string message = strerror(errno);
+    throw Napi::Error::New(env, message);
+  }
+
+  int counter = 0;
+  ssize_t index = 0;
+  while (index < buffer_size) {
+    size_t str_len = strlen(buffer_ptr + index);
+    Napi::String name = Napi::String::New(env, buffer_ptr + index, str_len);
+    result[counter++] = name;
+
+    index += str_len + 1;
+  }
+
+  return result;
+}
 
 static Napi::Value SetXattrSync(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
@@ -77,6 +119,8 @@ static Napi::Buffer<char> GetXattrSync(const Napi::CallbackInfo& info) {
 }
 
 static Napi::Object Init(Napi::Env env, Napi::Object exports) {
+  exports.Set(Napi::String::New(env, "listXattrSync"),
+              Napi::Function::New(env, ListXattrSync));
   exports.Set(Napi::String::New(env, "setXattrSync"),
               Napi::Function::New(env, SetXattrSync));
   exports.Set(Napi::String::New(env, "getXattrSync"),
